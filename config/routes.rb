e6a4276922c35f308d5421502a1211bc5e8eabb2
damionlowers <damion.lowers>
2014-12-26 23:45:27 -0500

    added custom rouths

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
   resources :statuses
+
   root to: 'statuses#index'
+  get 'feed', to: 'statuses#index', as: :feed
+
+  #devise_scope :user do
+  #  root to: "devise/sessions#new"
+  #end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
